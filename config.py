68838ff73a973868e32850fdfdfc279efe26d6ca
CatProgrammerYT <53112228+CatProgrammerYT@users.noreply.github.com>
2020-09-20 10:40:26 +0300

    Some useless updates

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Some bot settings
 settings = {
-    'token': 'NzU1NDQ1OTk0MTIwMDIwMTE5.X2DZ6A.D7reDzCvEeGqdEzNAYB4fvZ6ofE',
-    'bot': 'MeowBot [RU]',
-    'id': 755445994120020119,
-    'prefix': ','
+    #'token': 'Your token here', 
+    #'bot': 'Bot name here',
+    #'id': Bot id here,
+    #'prefix': 'Bot`s prefix'
 }
