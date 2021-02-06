9cd1f6042494ea4243201b01e0b5c2a09bcd7801
CatProgrammerYT <53112228+CatProgrammerYT@users.noreply.github.com>
2021-02-06 10:35:05 +0200

    Trying to getting jokes from api

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Some bot settings
 settings = {
-    'token': 'NzU1NDQ1OTk0MTIwMDIwMTE5.X2DZ6A.i4pEdw-KbYqrZuGigwc5jQXORZ4',
+    'token': 'NzU1NDQ1OTk0MTIwMDIwMTE5.X2DZ6A.AInDryuhriQD0dCH1Oby1i_LSR0',
     'bot': 'MeowBot [RU]',
     'id': 755445994120020119,
     'prefix': ','
